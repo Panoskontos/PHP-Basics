d72b5552746076593f0d2097d7ac75fef79c32d1
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-05-29 01:06:01 +0300

    t

--- fifth.php
+++ fifth.php
@@ -1,13 +1,18 @@
 <?
 
+// Class is a blueprint that you can use multiple times
 class Car{
     public $power = 100;
     private $cost = 22000;
     // constructor
-    public function __construct($name){
+    public function __construct($brand){
         echo "Constructor triggered";
         $this->power = 99;
-        $this->brand = $name;
+        $this->brand = $brand;
+    }
+    // methods
+    public function introduce(){
+        echo "This is a new ",$this->brand;
     }
 }
 
@@ -16,3 +21,20 @@ $c1 = new Car("Honda");
 echo "<br>";
 echo $c1->brand;
 echo $c1->power;
+$c1->introduce();
+echo var_dump($c1);
+echo "<br>";
+
+// Extending classes (Inheritance)
+class Benz extends Car{
+    public $year=2001;
+    public function date_created(){
+        echo "It was created $this->year";
+    }
+}
+
+$b1 = new Benz("Merecedes");
+echo "<br>";
+echo var_dump($b1);
+echo "<br>";
+$b1->date_created();
