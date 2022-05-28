69e2fdc265e8d9a3c918c37f99ffb5b1ae8cc23a
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-05-28 14:38:39 +0300

    first commit

--- main.php
+++ main.php
@@ -18,8 +18,36 @@
     ECHO "My first PHP script boy!";
     EchO "My first PHP script boy!";
 
-    // PHP Print
-    print "<br>This is fun"
+    // PHP Print embed html
+    print "<br>This is fun";
+    
+    print "<br> <br>";
+    
+    // var_dump for type
+    $x = 5985;
+    $x = 10.365;
+    $y = array("Volvo","BMW");
+    var_dump($x);
+    print "<br>";
+    var_dump($y);
+    print "<br>";
+
+    // Object
+    class Car {
+        public $color;
+        public $model;
+        public function __construct($color, $model) {
+          $this->color = $color;
+          $this->model = $model;
+        }
+        public function message() {
+          return "My car is a " . $this->color . " " . $this->model . "!";
+        }
+      }
+    
+    $c = new Car("WHITE","Corvette");
+    echo $c ->message();
+
     ?>
 
 </body>
