412446462a658ba0a07e3a4c84488701ac36599a
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-05-28 14:29:18 +0300

    first commit

---
+++ main.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    // variables
+    $txt = "Me";
+    echo "i love $txt";
+
+    $x = 4;
+    $y = 10;
+    echo $x+$y;
+    echo 'i am ',$y, ' how old are you?';
+
+    // PHP is case insencitive
+    echo "My first PHP script boy!";
+    ECHO "My first PHP script boy!";
+    EchO "My first PHP script boy!";
+
+    // PHP Print
+    print "<br>This is fun"
+    ?>
+
+</body>
+
+</html>
