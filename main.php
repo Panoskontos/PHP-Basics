0b4cab4dba7fad565c8aaa739632b8ebe5ff5c83
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-05-28 14:49:52 +0300

    first commit

--- main.php
+++ main.php
@@ -1,8 +1,3 @@
-<!DOCTYPE html>
-<html>
-
-<body>
-
     <?php
     // variables
     $txt = "Me";
@@ -10,46 +5,64 @@
 
     $x = 4;
     $y = 10;
-    echo $x+$y;
-    echo 'i am ',$y, ' how old are you?';
+    echo $x + $y;
+    echo 'i am ', $y, ' how old are you?';
 
     // PHP is case insencitive
     echo "My first PHP script boy!";
-    ECHO "My first PHP script boy!";
-    EchO "My first PHP script boy!";
+    echo "My first PHP script boy!";
+    echo "My first PHP script boy!";
 
     // PHP Print embed html
     print "<br>This is fun";
-    
+
     print "<br> <br>";
-    
+
     // var_dump for type
     $x = 5985;
     $x = 10.365;
-    $y = array("Volvo","BMW");
+    $y = array("Volvo", "BMW");
     var_dump($x);
     print "<br>";
     var_dump($y);
     print "<br>";
 
     // Object
-    class Car {
+    class Car
+    {
         public $color;
         public $model;
-        public function __construct($color, $model) {
-          $this->color = $color;
-          $this->model = $model;
+        public function __construct($color, $model)
+        {
+            $this->color = $color;
+            $this->model = $model;
         }
-        public function message() {
-          return "My car is a " . $this->color . " " . $this->model . "!";
+        public function message()
+        {
+            return "My car is a " . $this->color . " " . $this->model . "!";
         }
-      }
-    
-    $c = new Car("WHITE","Corvette");
-    echo $c ->message();
+    }
 
+    $c = new Car("WHITE", "Corvette");
+    echo $c->message();
+
+    // passing variables
+    $name = "Panos Kontos"
     ?>
 
-</body>
 
-</html>
+    <!DOCTYPE html>
+    <html>
+
+    <body>
+        <br>
+        <br>
+        <br>
+        <!-- Passing variables and html to php -->
+        <div style="color:white; background: grey; width:300px; height:200px;">
+            <?php echo "<h3> Name: $name </h3>"; ?>
+        </div>
+
+    </body>
+
+    </html>
