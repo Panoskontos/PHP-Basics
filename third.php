1066d5e9505a41ec8e74f66dc7d07b2dc7f4ac45
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-05-28 19:46:57 +0300

    s

--- third.php
+++ third.php
@@ -7,6 +7,51 @@ array_push($nums,"Go");
 array_pop($nums);
 echo var_dump($nums);
 
+echo "<br>";
+
+$chars = [
+    "goku" => [
+        "power"=>100,
+        "pride"=>0,
+        "fun"=>100,
+    ],
+    "vegeta" => [
+        "power"=>99,
+        "pride"=>1000,
+        "fun"=>0.001,
+    ],
+    "krillin" => [
+        "power"=>9,
+        "pride"=>4,
+        "fun"=>78,
+    ],
+];
+
+$chars["goku"]["power"] = 101;
+echo $chars["goku"]["power"];
+
+echo "<br>";
+
+// Loops
+// For loop
+for ($i =0; $i<10; $i++){
+    echo "$i ";
+}
+
+// while loop
+$i=0;
+while ($i < 10){
+    echo "<h3> $i </h3>";
+    $i++;
+}
+
+// Loop through array
+$anime = ["Naruto", "one piece","Death Note"];
+$j = 0;
+while ($j<count($anime)){
+    echo "<p>",$anime[$j],"</p>";
+    $j++;
+}
 
 ?>
 
